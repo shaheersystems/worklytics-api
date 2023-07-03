f17ba661379bc7cb2689165f92f5e8df4007d374
Ghani <ghani@eyrox.com>
2023-07-04 00:39:11 +0500

    end point function

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify,request, render_template
 from flask_restful import Api
 from database import db
 from resources import routes
+from database.models import Company
 
 dbName = "job_db"
 
@@ -20,6 +21,22 @@ routes.intialize_routes(api)
 def index():
     return "<h1>Worklytics Api</h1>"
 
+@app.route('/api/companies', methods=['POST'])
+def return_companies():
+    companies = Company.objects().all()
+    company_list = []
+    for company in companies:
+        company_data = {
+            'name': company.name,
+            'location': company.location,
+            'industry': company.industry,
+            'user':company.user_id,
+            'website': company.website,
+            'founded_year': company.founded_year,
+            'hrName': company.hrName
+        }
+        company_list.append(company_data)
+    return jsonify(company_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
